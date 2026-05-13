b2adda53b4f8ae3c09e2485c5679845793dff4cc
nel <neeeel@qq.com>
2026-05-13 11:31:22 +0800

    添加 vite-plugin-singlefile 插件以支持单文件构建

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    viteSingleFile(),
   ],
-  base: '/how-much/',
+  base: './',
+  build: {
+    assetsInlineLimit: 1024 * 1024, // 1MB，强制所有资源内联为 base64
+  },
 })
